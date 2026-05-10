9d89aad59c7754e08dea0a5cec71f14e7a3576fd
Lyeon <devlyeon@gmail.com>
2026-05-10 18:36:54 +0900

    fix resolution bug

--- Assets/Scripts/Scene/Splash.cs
+++ Assets/Scripts/Scene/Splash.cs
@@ -6,8 +6,21 @@ public class Splash : MonoBehaviour
     {
         Data<FullScreenSetting> fullScreen = new("displayPref_01.dat");
         Data<ResolutionSetting> resolution = new("displayPref_02.dat");
-        bool isFullScreen = fullScreen.Read().isFullScreen;
-        ResolutionSetting resolutionValue = resolution.Read();
+        bool isFullScreen;
+        ResolutionSetting resolutionValue;
+        if (fullScreen.Exists()) isFullScreen = fullScreen.Read().isFullScreen;
+        else
+        {
+            isFullScreen = true;
+            fullScreen.Write(new FullScreenSetting(){isFullScreen = true});
+        }
+        if (resolution.Exists()) resolutionValue = resolution.Read();
+        else
+        {
+            resolutionValue = new ResolutionSetting(){width=1920, height=1080};
+            resolution.Write(resolutionValue);
+        }
         Screen.SetResolution(resolutionValue.width, resolutionValue.height, isFullScreen);
+        
     }
 }
